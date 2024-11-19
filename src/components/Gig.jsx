c72118f74f48dde851cb96c8638b05df2dcad4d9
Kelly <kellyhowes83@gmail.com>
2024-11-19 10:44:03 +0000

    Added the word favourite so it was understandable what the star button did

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -13,6 +13,7 @@ const Gig = (props) => {
     <p className="event-description">{props.description}</p>
     <p className="date-time">{props.dateTime}</p>
     <p className="location">{props.location}</p>
+    <p>Favourite</p>
     <button className="fav-button" onClick={favourite}><img className="favourite" src={fav ? "src/assets/full_star.png" : "src/assets/star.png"}/></button>
     <hr/>
     </>
